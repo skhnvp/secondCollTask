2cbcea2fcd0f00e1f4b62ea9453a552a0eb442c0
Pavel <skhnvp@gmail.com>
2025-04-03 00:17:06 +0300

    init

---
+++ src/main/java/ru/stepup/Main.java
@@ -0,0 +1,76 @@
+package ru.stepup;
+
+/*
+Перед вами код метода: реализации сортировки «пузырьком».
+
+Перепишите код, чтобы вместо списка ArrayList<Integer> intList использовался массив int[] intArray.
+
+public static void bubbleSort(ArrayList<Integer> intList) {
+for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
+            }
+        }
+}
+
+public static void bubbleSort(int[] intArray) {
+	// Реализовать сортировку для массива
+}
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(6);
+        list.add(5);
+        list.add(3);
+        list.add(1);
+        list.add(10);
+        list.add(8);
+        list.add(2);
+        list.add(4);
+        list.add(7);
+        list.add(9);
+
+        System.out.println("List: " + list);
+        bubbleSort(list);
+
+        int[] intArray = {6,5,3,1,10,8,2,4,7,9};
+
+        System.out.println("array: " + Arrays.toString(intArray));
+        bubbleSort(intArray);
+    }
+    public static void bubbleSort(ArrayList<Integer> intList) {
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
+            }
+        }
+        System.out.println("Sorted: " + intList);
+    }
+
+    public static void bubbleSort(int[] intArray) {
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int j = 0; j < intArray.length - i - 1; j++) {
+                if (intArray[j] > intArray[j+1]) {
+                    int temp = intArray[j];
+                    intArray[j] = intArray[j+1];
+                    intArray[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted: " + Arrays.toString(intArray));
+    }
+}
